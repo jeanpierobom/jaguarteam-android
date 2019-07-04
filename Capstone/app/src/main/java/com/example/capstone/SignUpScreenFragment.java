0785653c42ec8d3de004b5d5c5b185e03059b321
Anderson Borba <andersonhborba@hotmail.com>
2019-07-04 15:17:18 -0700

    started implementing signup api post

--- Capstone/app/src/main/java/com/example/capstone/SignUpScreenFragment.java
+++ Capstone/app/src/main/java/com/example/capstone/SignUpScreenFragment.java
@@ -3,12 +3,18 @@ package com.example.capstone;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SignUpScreenFragment extends Fragment {
+    private String userType = "student";
+    private TextInputEditText fullName;
+    private TextInputEditText password;
+    private TextInputEditText confirmPassword;
+    private TextInputEditText email;
 
     @Nullable
     @Override
