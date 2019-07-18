c4ad8b9215103981243d243f9f7f7909374cf00a
Jean Pierobom <pierobom@gmail.com>
2019-07-18 11:47:30 -0700

    Implemented POST method in API caller, URL standard, storing token, login with Outcom

--- Capstone/app/src/main/java/com/example/capstone/SignInScreenFragment.java
+++ Capstone/app/src/main/java/com/example/capstone/SignInScreenFragment.java
@@ -4,6 +4,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,35 +18,34 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.codelabs.appauth.OutcomAppAuthInfo;
 
 import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignInScreenFragment extends Fragment {
 
-
-    private Button button;
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private Button signInButton;
     private SignInButton signInGoogleButton;
     private TextView signUpButton;
-    //private Button toTeacherProfileConfirmation;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sign_in_layout,container,false);
 
+        emailInput = view.findViewById(R.id.sign_in_input_email);
+        passwordInput = view.findViewById(R.id.sign_in_input_password);
         signUpButton = view.findViewById(R.id.to_sign_up);
         signInGoogleButton = view.findViewById(R.id.sign_in_google_button);
-        //toTeacherProfileConfirmation = view.findViewById(R.id.to_teacher_profile_confirmation);
-        button = view.findViewById(R.id.sign_in_button);
-
-//        toTeacherProfileConfirmation.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Navigation.findNavController(getActivity(), R.id.navHostFragment).navigate(R.id.action_sign_in_screen_to_teacher_profile_confirmation);
-//            }
-//        });
+        signInButton = view.findViewById(R.id.sign_in_button);
 
         TextView textView = (TextView) signInGoogleButton.getChildAt(0);
         textView.setText("Sign In With Google");
@@ -57,10 +58,39 @@ public class SignInScreenFragment extends Fragment {
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Navigation.findNavController(getActivity(),R.id.navHostFragment).navigate(R.id.action_sign_in_screen_to_student_home_search);
+            //String email = "student@outcom.io";
+            //String password = "secret";
+            String email = emailInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            // Build params
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("email", email);
+            params.put("password", password);
+
+            try {
+                APICaller.Post("login", params, new APICallBack() {
+                    @Override
+                    public void callBack(JsonReader jsonObject) {
+                        try {
+                            LoginDTO dto = JsonAPIParser.ParseLoginRequest(jsonObject);
+                            OutcomAppAuthInfo.getInstance().setLoginInfo(dto, "Token retrieved successfully");
+                            Log.e("TEST POST REQUEST","token: " + dto.getToken());
+                            Navigation.findNavController(getActivity(),R.id.navHostFragment).navigate(R.id.action_sign_in_screen_to_student_home_search);
+                        } catch(Exception e) {
+                            Log.e("TEST GET REQUEST","Error while parsing token: " + e);
+                            OutcomAppAuthInfo.getInstance().setLoginInfo(null, "Error to retrieve token: " + e.getMessage());
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                Log.e("TEST POST REQUEST","An exception has occurred: " + e);
+            } finally {
+                Log.e("TEST POST REQUEST","This is finally");
+            }
             }
         });
 
