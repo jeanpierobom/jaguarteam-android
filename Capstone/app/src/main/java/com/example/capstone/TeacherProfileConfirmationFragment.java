d5b8be56e413533ce74e4e5f702bb65e10481ae6
Gianlucci Badiali Minarelli <gianlucci.minarelli@gmail.com>
2019-07-23 18:23:54 -0700

    Check parameters before sending POST request

--- Capstone/app/src/main/java/com/example/capstone/TeacherProfileConfirmationFragment.java
+++ Capstone/app/src/main/java/com/example/capstone/TeacherProfileConfirmationFragment.java
@@ -30,6 +30,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -85,7 +86,8 @@ public class TeacherProfileConfirmationFragment extends Fragment implements OnMa
 
         selectedTeacher = ((MainActivity)getActivity()).getTeacher();
         selectedAvailability = ((MainActivity)getActivity()).getAvailability();
-        selectedClassType = ((MainActivity)getActivity()).getClassTypes().get(0);
+        // Checking if selectedClassType is not null here.
+        selectedClassType = ((MainActivity)getActivity()).getClassTypes().size() == 0 ? "" : ((MainActivity)getActivity()).getClassTypes().get(0);
 
         Log.d("POST_Date", selectedAvailability.getDate());
         Log.d("POST_TimeSlots", selectedAvailability.getTimeSlots().toString());
@@ -185,17 +187,31 @@ public class TeacherProfileConfirmationFragment extends Fragment implements OnMa
 
     public void sendRequest (Double[] locationCoordinates, String locationName, String inputMessage) {
 
+        // Checking if the parameters we need were initialized. If not, send an empty string.
+        // If the parameter type is String, it will default to the empty string so there is no need to check it here.
+        // Note: selectedClassType has already been checked.
+        String teacherIdParam = selectedTeacher.getId() == 0 ? "" : Integer.toString(selectedTeacher.getId());
+        String timeSlotParam = selectedAvailability.getTimeSlots().size() == 0 ? "" : selectedAvailability.getTimeSlots().get(0);
+        String locationLatParam = "", locationLongParam = "";
+        if (locationCoordinates.length == 2) {
+            locationLatParam = locationCoordinates[0].toString();
+            locationLongParam = locationCoordinates[1].toString();
+        } else {
+            Log.e("Error", "Location coordinates array doesn't have two elements");
+        }
+        String priceParam = selectedTeacher.getHourlyRate() == 0.0f ? "" : Float.toString(selectedTeacher.getHourlyRate());
+
         Map<String, String> params = new HashMap<String, String>();
         params.put("studentId", "1");
-        params.put("teacherId", Integer.toString(selectedTeacher.getId()));
+        params.put("teacherId", teacherIdParam);
         params.put("date", selectedAvailability.getDate());
-        params.put("startTime", selectedAvailability.getTimeSlots().get(0));
+        params.put("startTime", timeSlotParam);
         params.put("duration", "01:00");
         params.put("classType", selectedClassType);
         params.put("location", locationName);
-        params.put("locationLat", locationCoordinates[0].toString());
-        params.put("locationLong", locationCoordinates[1].toString());
-        params.put("price", Float.toString(selectedTeacher.getHourlyRate()));
+        params.put("locationLat", locationLatParam);
+        params.put("locationLong", locationLongParam);
+        params.put("price", priceParam);
         params.put("message", inputMessage);
 
         try {
